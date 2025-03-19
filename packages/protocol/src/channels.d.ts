b2e6cb5e8b75dbdc936dbd7cac309d088c78e5d9
Krishna Suravarapu <36037520+KrishnaSuravarapu@users.noreply.github.com>
2025-03-19 14:08:19 +0530

    Chore: AutoCommit fixes

--- packages/protocol/src/channels.d.ts
+++ packages/protocol/src/channels.d.ts
@@ -988,7 +988,6 @@ export type BrowserTypeLaunchOptions = {
   chromiumSandbox?: boolean,
   firefoxUserPrefs?: any,
   slowMo?: number,
-  useDefaultContext?: boolean,
 };
 export type BrowserTypeLaunchResult = {
   browser: BrowserChannel,
